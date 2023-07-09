fc8675d6803a1a03ba2f8d000c1e7c9cc39fe68b
bonniekang <jihye59kang@gmail.com>
2023-07-09 18:00:35 +0900

    Add : basic layout & necessary pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,23 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
 
-import { Button } from "@components/ui/button";
+import { Button } from '@components/ui/button'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-center p-10 ${inter.className}`}
     >
-      <Button>블로그</Button>
+      <h1 className="py-10 text-3xl">블로그입니다.</h1>
+      <div className="flex items-center justify-center gap-5 flex-wrap">
+        <Button>PROJECTS</Button>
+        <Button>POSTS</Button>
+        <Button>ARCHIVE</Button>
+        <Button>RESUME</Button>
+        <Button>SOCIAL</Button>
+      </div>
     </main>
-  );
+  )
 }
---
+++ pages/posts/[id].tsx
@@ -0,0 +1,7 @@
+export default function PostPage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-10">
+      <h1 className="py-10 text-3xl">PostPage</h1>
+    </main>
+  )
+}
