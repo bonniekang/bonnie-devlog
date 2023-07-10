f9a93496c3ff6cceece0c84ba7e87ecdbd135f47
bonniekang <jihye59kang@gmail.com>
2023-07-10 13:11:37 +0900

    Add : show all posts & individual post

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,7 +1,34 @@
-export default function PostPage() {
+import { getPosts, getPost } from '../../utils/mdxUtils'
+
+export default function PostPage(props: any) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-10">
       <h1 className="py-10 text-3xl">PostPage</h1>
+      <div dangerouslySetInnerHTML={{ __html: props.content.toString() }} />
     </main>
   )
+}
+
+export async function getStaticPaths() {
+  const ids = getPosts().map((post) => {
+    return {
+      params: {
+        id: post.id,
+      },
+    }
+  })
+
+  return {
+    paths: ids,
+    fallback: false,
+  }
+}
+
+export async function getStaticProps({ params }: any) {
+  const { id } = params
+  const post = await getPost(id)
+
+  return {
+    props: post,
+  }
 }
--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,7 +1,27 @@
-export default function PostsPage() {
+import Link from 'next/link'
+
+import { getPosts } from '../../utils/mdxUtils'
+
+export default function PostsPage({ posts }: any) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-10">
-      <h1 className="py-10 text-3xl">PostsPage</h1>
+      <h1 className="py-10 text-3xl">Posts</h1>
+      {posts.map((post: any) => (
+        <div key={post.id}>
+          <Link href={`/posts/${post.id}`}>
+            <h2>{post.frontmatter.title}</h2>
+          </Link>
+        </div>
+      ))}
     </main>
   )
+}
+
+export async function getStaticProps() {
+  const posts = getPosts()
+  return {
+    props: {
+      posts,
+    },
+  }
 }
