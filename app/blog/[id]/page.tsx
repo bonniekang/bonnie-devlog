c4922c4ce1fb8c01bb8d174480fb07e67def2f26
bonniekang <jihye59kang@gmail.com>
2024-10-03 23:57:09 +0900

    Fix : table markdown error

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -2,7 +2,12 @@ import { unified } from 'unified'
 import remarkParse from 'remark-parse'
 import remarkGfm from 'remark-gfm'
 import remarkRehype from 'remark-rehype'
-import html from 'rehype-stringify'
+import rehypeStringify from 'rehype-stringify'
+import rehypeRaw from 'rehype-raw'
+import Markdown from 'react-markdown'
+import remarkHtml from 'remark-html'
+import rehypeHighlight from 'rehype-highlight'
+import remarkBreaks from 'remark-breaks'
 
 import { getNotionPosts } from '@/lib/notion/client'
 
@@ -10,13 +15,15 @@ export default async function BlogPostPage({ params }: { params: { id: string } 
   const blogPostId = params.id
 
   const postContent = await getNotionPosts(blogPostId)
-
   const file = await unified()
-    .use(remarkParse)
-    .use(remarkGfm)
-    .use(remarkRehype)
-    .use(html)
-    .process(postContent)
+    .use(remarkParse) // Parse markdown
+    .use(remarkGfm) // GitHub Flavored Markdown for tables
+    .use(remarkBreaks)
+    .use(remarkRehype) // Convert markdown to HTML
+    .use(rehypeRaw)
+    .use(rehypeHighlight)
+    .use(rehypeStringify) // Convert HTML AST to HTML string
+    .process(postContent) // Process the markdown
 
   return <div dangerouslySetInnerHTML={{ __html: file.value }} />
 }
