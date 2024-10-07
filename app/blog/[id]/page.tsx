30cbb6023c31c190cde0c9dbb13a582e11ab5f7b
bonniekang <jihye59kang@gmail.com>
2024-10-07 23:59:45 +0900

    Fix : plugin incompatibility

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,12 +1,6 @@
-import { unified } from 'unified'
-import remarkParse from 'remark-parse'
 import remarkGfm from 'remark-gfm'
-import remarkRehype from 'remark-rehype'
-import rehypeStringify from 'rehype-stringify'
-import rehypeRaw from 'rehype-raw'
-import rehypeHighlight from 'rehype-highlight'
-import remarkBreaks from 'remark-breaks'
-import ReactMarkdown from 'react-markdown'
+
+import Markdown from 'react-markdown'
 
 import { getNotionPosts } from '@/lib/notion/client'
 
@@ -14,15 +8,10 @@ export default async function BlogPostPage({ params }: { params: { id: string } 
   const blogPostId = params.id
 
   const postContent = await getNotionPosts(blogPostId)
-  // const file = await unified()
-  //   .use(remarkParse) // Parse markdown
-  //   .use(remarkGfm) // Support GitHub Flavored Markdown (tables, etc.)
-  //   .use(remarkBreaks) // create line breaks in markdown
-  //   .use(remarkRehype) // Convert markdown to HTML
-  //   .use(rehypeRaw) // Process raw HTML in markdown
-  //   .use(rehypeHighlight) // Add syntax highlighting to code blocks
-  //   .use(rehypeStringify) // Convert HTML AST to HTML string
-  //   .process(postContent) // Process the markdown
 
-  return <ReactMarkdown className={'flex-col'}>{postContent}</ReactMarkdown>
+  return (
+    <Markdown className={'flex-col'} remarkPlugins={[remarkGfm]} rehypePlugins={[]}>
+      {postContent}
+    </Markdown>
+  )
 }
