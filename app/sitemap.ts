8a70fa30bc09338473b351f2109bdd0d96b7835e
bonniekang <jihye59kang@gmail.com>
2025-01-14 20:23:03 +0900

    Chore

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,7 +6,7 @@ export default async function sitemap() {
   const blogPostList = (await getBlogPostList()) as TBlogPostList[]
   const posts = blogPostList.map(({ id, properties }) => ({
     url: `${BASE_URL}/blog/${id}`,
-    lastModified: properties.published.date.start,
+    lastModified: properties.published.date.start || new Date().toISOString().split('T')[0],
   }))
 
   const blogTags = extractUniqueTags(blogPostList).map((tagName) => ({
